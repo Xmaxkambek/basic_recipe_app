fcccef4868eb690008e571d7f0263d6c5ae6e186
ZARIFMAC <uzbzarif@gmail.com>
2022-11-04 15:03:19 +0500

    reorginze

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:basic_recipe_app/screen/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
   ));
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
-    );
-  }
 }
