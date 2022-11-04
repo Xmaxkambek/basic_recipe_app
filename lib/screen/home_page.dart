bbd79b86daf9002fc1e760f3a1e9cac2d3dfb856
ZARIFMAC <uzbzarif@gmail.com>
2022-11-04 16:20:41 +0500

    avatar image

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -6,24 +6,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 40, 145, 43),
+        backgroundColor: Color.fromARGB(255, 62, 199, 119),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('images/vegtables.jpg'),
-                    fit: BoxFit.cover),
-                borderRadius: BorderRadius.circular(100),
-                
-              ),
+            CircleAvatar(
+              backgroundImage: AssetImage('images/vegtables.jpg'),
+              radius: 80,
             ),
             Column(
               children: [
-                SizedBox(height: 40,),
+                SizedBox(
+                  height: 40,
+                ),
                 Container(
                     alignment: Alignment.center,
                     child: Text(
@@ -32,21 +27,15 @@ class HomePage extends StatelessWidget {
                           fontWeight: FontWeight.w800,
                           fontSize: 30,
                           color: Colors.white),
-                    )),SizedBox(height: 40,),
-                TextButton(
-                    onPressed: () {},
-                    child: Container(
-                      
-                      width: 200,
-                      height: 50,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(color: Colors.white,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Text(
-                        'Get a meal',
-                        style: TextStyle(color: Colors.black, fontSize: 20),
-                      ),
-                    ))
+                    )),
+                SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print('Pressed');
+                    },
+                    child: Text('Get a meal'))
               ],
             )
           ],
