be28be7cf0546a9219ddf490a3d8d9f224e4fe74
ZARIFMAC <uzbzarif@gmail.com>
2022-11-04 16:32:45 +0500

    navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:basic_recipe_app/screen/menu_page.dart';
 import 'package:flutter/material.dart';
 import 'package:basic_recipe_app/screen/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    initialRoute: 'home',
+    routes: {
+      'home': (context) => HomePage(),
+      'menu': (context) => MenuPage(),
+    },
   ));
 }
