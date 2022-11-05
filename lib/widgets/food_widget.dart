0a91cb9f50185497de71b5693ba8620ca340b416
ZARIFMAC <uzbzarif@gmail.com>
2022-11-05 16:03:38 +0500

    fix

--- lib/widgets/food_widget.dart
+++ lib/widgets/food_widget.dart
@@ -3,8 +3,8 @@ import 'package:basic_recipe_app/food.dart';
 import 'package:flutter/material.dart';
 
 class FoodWidget extends StatelessWidget {
-  Food food = Food(name: 'Somsa', img: 'Somsa.png', price: 1);
-  FoodWidget({Key? key, required this.food}) : super(key: key);
+  final Food food;
+  const FoodWidget({Key? key, required this.food}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
